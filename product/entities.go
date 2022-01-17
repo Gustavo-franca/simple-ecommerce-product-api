c5e26b5328466ffd844063f5bc411a85e44009fd
Gustavo França <gustavocandidofranca@gmail.com>
2022-01-17 20:56:38 -0300

    Add mysql database

--- product/entities.go
+++ product/entities.go
@@ -1,8 +1,14 @@
 package product
 
+import "gorm.io/gorm"
+
 type (
 	Entity struct {
 		ID          string
 		Description string
 	}
 )
+
+func Migrate(db *gorm.DB) error {
+	return db.AutoMigrate(&Entity{})
+}
