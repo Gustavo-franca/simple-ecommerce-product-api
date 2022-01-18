31012adec4c7e99a0babe407e6c24c6b056071ab
Gustavo França <gustavocandidofranca@gmail.com>
2022-01-18 20:15:49 -0300

    Add new product params

--- product/entities.go
+++ product/entities.go
@@ -1,17 +1,45 @@
 package product
 
-import "gorm.io/gorm"
+import (
+	"database/sql/driver"
+	"errors"
+	"gorm.io/gorm"
+	"strings"
+)
 
 type (
 	Entity struct {
-		ID          string
-		Description string
+		ID            string  `json:"id,omitempty"`
+		Title         string  `json:"title,omitempty"`
+		Description   string  `json:"description,omitempty"`
+		Price         float64 `json:"price,omitempty"`
+		PreviousPrice float64 `json:"previousPrice,omitempty"`
+		Rate          float64 `json:"rate,omitempty"`
+		ShippingPrice float64 `json:"shippingPrice,omitempty"`
+		Stock         uint64  `json:"stock,omitempty"`
+		ImgURL        string  `json:"imgUrl,omitempty"`
+		Images        Images  `json:"images,omitempty" gorm:"size:10000"`
 	}
+	Images []string
 )
 
 func (Entity) TableName() string {
 	return "products"
 }
+
 func Migrate(db *gorm.DB) error {
 	return db.AutoMigrate(&Entity{})
+}
+
+func (i Images) Value() (driver.Value, error) {
+	return strings.Join(i, ";;;"), nil
+}
+
+func (i *Images) Scan(data interface{}) error {
+	strInBytes, ok := data.([]byte)
+	if !ok {
+		return errors.New("fail in scan data")
+	}
+	*i = strings.Split(string(strInBytes), ";;;")
+	return nil
 }
